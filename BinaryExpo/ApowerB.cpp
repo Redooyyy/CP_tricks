f1e0ac65b0d63e7f7e4a328d4161881640666224
Redooyyy <rwarieal@gmail.com>
2026-02-05 18:38:37 +0600

    added modulo for large number

--- BinaryExpo/ApowerB.cpp
+++ BinaryExpo/ApowerB.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int M = 1e9 + 7;
 
 long long my_pow(int a, int b) {
   if (b == 0)
@@ -11,9 +12,20 @@ long long my_pow(int a, int b) {
     return store * store;
   }
 }
+long long withModulo(int a, int b) {
+  if (b == 0)
+    return 1;
+  long long int store = my_pow(a, b / 2);
+  if (b & 1) {
+    return (a * (store * 1LL * store) % M) % M; // formula in my note
+  } else {
+    return (store * 1LL * store) % M;
+  }
+}
 
 int main() {
   int a, b;
   cin >> a >> b;
-  cout << my_pow(a, b);
+  // cout << my_pow(a, b);
+  cout << withModulo(a, b);
 }
